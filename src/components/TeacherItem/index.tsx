1dced6bd9e5ab87913618509c8386692136e7f8a
erikalbuquerque <eriksilv.77@gmail.com>
2020-08-04 17:27:12 -0300

    add styles, TeacherItem and styles TeacherList

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/39579838?s=460&u=b595e18b727d76ba2c2141c2890d39eae37a5db3&v=4"
+          alt="Érik Albuquerque"
+        />
+        <div>
+          <strong>Érik Albuquerque</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 20, 00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
--- src/pages/TeachForm/index.tsx
+++ src/pages/TeachForm/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 function TeachForm() {
   return (
-    <div>
-      <h1>Teach</h1>
-      <h1>List</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível quer você quer dar aulas." />
     </div>
   );
 }
